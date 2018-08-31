bdcd56fc7f91d4e285ad9e97a3f4f3dd0973781d
Kenichiro Sato <ilove_soccer_iam_ken@icloud.com>
2018-08-31 21:17:56 +0900

    GoForwardUnityChan 初コミット
    音に非常に苦労した

---
+++ Assets/CubeCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CubeCollision : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+
+	void Start(){ 
+		audioSource = gameObject.GetComponent<AudioSource> ();
+	}
+	void OnCollisionEnter2D (Collision2D other){
+
+		Debug.Log ("Hello");
+			if(other.gameObject.tag == "CubePrefab")
+			{
+			audioSource.Play();
+		}else if(other.gameObject.tag == "ground"){
+			audioSource.Play();
+		}
+	}
+
+}
